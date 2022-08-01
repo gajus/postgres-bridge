d6c144d9a3944187a320adecf6b197e529310505
Gajus Kuizinas <gajus@gajus.com>
2022-08-01 18:22:44 -0500

    test: test both packages at once

--- test/postgres-bridge/bridge.ts
+++ test/postgres-bridge/bridge.ts
@@ -1,29 +1,64 @@
 import test from 'ava';
+import {
+  Pool,
+} from 'pg';
 import postgres from 'postgres';
+import * as sinon from 'sinon';
 import {
   bridge,
 } from '../../src/bridge';
 
-test('query method', async (t) => {
-  const pool = bridge(postgres, {
-    user: 'postgres',
+const clients = [
+  'pg',
+  'postgres',
+];
+
+const createPool = (clientName: string, poolConfiguration) => {
+  if (clientName === 'pg') {
+    return new Pool(poolConfiguration);
+  }
+
+  return bridge(postgres, {
+    ...poolConfiguration,
+  });
+};
+
+for (const client of clients) {
+  test(client + ': connect event is fired when a new connection is made', async (t) => {
+    const pool = createPool(client, {
+      user: 'postgres',
+    });
+
+    const spy = sinon.spy();
+
+    pool.on('connect', spy);
+
+    const connection = await pool.connect();
+
+    t.true(spy.called);
+
+    t.is(spy.firstCall.args[0], connection);
   });
 
-  const connection = await pool.connect();
+  test(client + ': query method', async (t) => {
+    const pool = createPool(client, {
+      user: 'postgres',
+    });
 
-  const result = await connection.query('SELECT 1');
+    const connection = await pool.connect();
 
-  t.is(result.rows.length, 1);
-  t.is(result.command, 'SELECT');
-  t.deepEqual(result.rows, [
-    {
-      '?column?': 1,
-    },
-  ]);
-  t.deepEqual(result.fields, [
-    {
-      dataTypeID: 23,
-      name: '?column?',
-    },
-  ]);
-});
+    const result = await connection.query('SELECT 1');
+
+    t.is(result.rows.length, 1);
+    t.is(result.command, 'SELECT');
+    t.like(result.rows[0],
+      {
+        '?column?': 1,
+      });
+    t.like(result.fields[0],
+      {
+        dataTypeID: 23,
+        name: '?column?',
+      });
+  });
+}
